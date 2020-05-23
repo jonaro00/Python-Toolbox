a26cab6a5db8a58b17bd7b983fc7af36da6e156b
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2020-05-23 15:55:34 +0200

    Initial, added timer & counter

---
+++ tools.py
@@ -0,0 +1,21 @@
+from time import time
+
+def timer(func, *args, **kwargs):
+    """Decorator to time the call of a function"""
+    def f(*args, **kwargs):
+        before = time()
+        rv = func(*args, **kwargs)
+        after = time()
+        print(f'{func.__name__} executed in {after - before} s')
+        return rv
+    return f
+
+def counter(func, *args, **kwargs):
+    """Decorator that counts the number of calls to a function"""
+    func._count = 0
+    def f(*args, **kwargs):
+        rv = func(*args, **kwargs)
+        func._count += 1
+        print(f'{func.__name__} has been called {func._count} times')
+        return rv
+    return f
